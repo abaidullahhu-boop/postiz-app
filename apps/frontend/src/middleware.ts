f63d9f5ce9eec219c16725e3a053ec71e19b7a8c
abaid hu <abaid.ullah.hu@gmail.com>
2025-10-17 20:52:14 +0500

    redirection for home fixed

--- apps/frontend/src/middleware.ts
+++ apps/frontend/src/middleware.ts
@@ -43,7 +43,8 @@ export async function middleware(request: NextRequest) {
   if (
     nextUrl.pathname.startsWith('/uploads/') ||
     nextUrl.pathname.startsWith('/p/') ||
-    nextUrl.pathname.startsWith('/icons/')
+    nextUrl.pathname.startsWith('/icons/') ||
+    nextUrl.pathname.startsWith('/home')
   ) {
     return topResponse;
   }
